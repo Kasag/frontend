8c3188ff547c5ddf330664b93f0d68c69ef3e878
MOGINodoka <umbrellaislandwa@gmail.com>
2024-01-08 22:24:09 +0000

    Add new files and make code changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  // api/〇〇のパスにアクセスしたらhttp://host.docker.internal:8000/api/〇〇/にリクエストを送る
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://host.docker.internal:8000/api/:path*/',
+      },
+    ]
+  },
+}
